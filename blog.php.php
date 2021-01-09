57f6b95accb638d7cfc931d7f331cfa9d58611b6
Lokesh kumar <lokesh.kmar19@gmail.com>
2021-01-09 15:15:17 +0530

    Add files via upload

---
+++ blog.php.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Blog - Business Casual - Start Bootstrap Theme</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="css/business-casual.css" rel="stylesheet">
+
+    <!-- Fonts -->
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800" rel="stylesheet" type="text/css">
+    <link href="http://fonts.googleapis.com/css?family=Josefin+Slab:100,300,400,600,700,100italic,300italic,400italic,600italic,700italic" rel="stylesheet" type="text/css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <div class="brand">Business Casual</div>
+    <div class="address-bar">12 | SOUTH WEST | DELHI, PIN CODE-111000| 123.456.7890</div>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default" role="navigation">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <!-- navbar-brand is hidden on larger screens, but visible when the menu is collapsed -->
+                <a class="navbar-brand" href="index.html">Business Casual</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="about.php">About</a>
+                    </li>
+                    <li>
+                        <a href="blog.php">Blog</a>
+                    </li>
+                    <li>
+                        <a href="contact.php">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="container">
+
+        <div class="row">
+            <div class="box">
+                <div class="col-lg-12">
+                    <hr>
+                    <h2 class="intro-text text-center">Company
+                        <strong>blog</strong>
+                    </h2>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="img/slide-1.jpg" alt="">
+                    <h2>
+                        <br>
+                        <small>JUlY 13, 2020</small>
+                    </h2>The age of innocenec
+                    <p>Even in 1993, it seemed surprising that Martin Scorsese should direct an adaptation of The Age of Innocence. Why was the director of bloody and furious classics such as Taxi Driver and Raging Bull taking on this story of decorum and reserve in New York high society? When the film came out, critic Roger Ebert wrote that the pairing had “struck many people as astonishing – as surprising, say, as if Abel Ferrara had announced a film by Henry James”.
+                    But Edith Wharton’s great novel has more in common with Scorsese’s work, especially Mean Streets and Goodfellas, than might be supposed. Most obviously, it’s about gangs: their unspoken rules, their codes of honour, and their structures of power. Wharton’s society, with the formidable Van der Luydens “above all of them”, is as tough as they come. Transgressors against its strict honour code are punished without mercy. Scorsese told Ebert</p>
+                    <a href="#" class="btn btn-default btn-lg">Read More</a>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full" src="https://blogstorage.s3.amazonaws.com/uploads/pinax-images/image-set-99180/aee65b0b-f224-4dcb-bceb-8e3e4c5d3452.jpg" alt="">
+                    <h2>Three takeaways as Nashville SC cruises past D.C. United
+                        <br>
+                        <small>JULY 15, 2020</small>
+                    </h2>
+                    <p>The scoreline may not reflect the same sentiment, but Nashville SC's 1-0 win over D.C. United on Wednesday was about as easy as a professional football match can get.
+                    Nashville saw strikers Dominique Badji and Daniel Rios start a match together for the first time in MLS last night and unfortunately, Badji did not make it past the half hour mark. He was pulled off by manager Gary Smith with what appeared to be a hamstring problem and was replaced by Alan Winn who made his second consecutive appearance.
+                    The lone goal in this match came from the Mexican international, Rios, came in the 70th minute off of, you guessed it, a set piece. The corner from Alistair Johnston fell perfectly to Dave Romney who redirected the ball to Rios for the goal.
+                    Here are my three takeaways from the contest</p>
+                    <a href="#" class="btn btn-default btn-lg">Read More</a>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <img class="img-responsive img-border img-full"  src="https://m.economictimes.com/thumb/msid-74458051,width-1200,height-900,resizemode-4,imgsize-674179/untitled-5jik.jpg" alt="">
+                    <h2>NO TIME TO DIE OFFICIAL TRAILER 2
+                        <br>
+                        <small>JULY 20, 2020</small>
+                    </h2>
+                    <p>The mission that changes everything begins as MGM Studios (and Universal Pictures) presents the second official trailer for the upcoming Bond film….No Time to Die. View trailer below.
+                       Five years after the capture of Ernst Stavro Blofeld, James Bond has left active service. He is approached by Felix Leiter, his friend and a CIA officer, who enlists his help in the 
+                      search for Valdo Obruchev, a missing scientist. When it becomes apparent that Obruchev was abducted, Bond must confront a danger the likes of which the world has never seen before.</p>
+                       Oh wow….I was waiting (and hoping) for a new trailer for this movie. Given the effects of what has happened to Hollywood and movie theaters due to the COVID-19 pandemic, I was praying that 
+                      No Time to Die was gonna be released later this year and not delayed further. Thankfully, this trailer proves that the movie is gonna be released in November….hopefully. Still, regardless of 
+                  that fact, this new trailer certainly does look quite impressive and has quite an action packed feeling more so than what was presented in Spectre. Definitely looks engaging and I hope its to be a good James Bond flick for Daniel Craig’s last outing as the British super spy.
+              <a href="#" class="btn btn-default btn-lg">Read More</a>
+                    <hr>
+                </div>
+                <div class="col-lg-12 text-center">
+                    <ul class="pager">
+                        <li class="previous"><a href="#">&larr; Older</a>
+                        </li>
+                        <li class="next"><a href="#">Newer &rarr;</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <p>Copyright &copy; Your Website 2014</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
